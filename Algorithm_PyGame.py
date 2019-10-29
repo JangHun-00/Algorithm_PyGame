2613735c4502f8d42561f8418b5be8f7a75ccd8b
Jang Hun <janghun0056@gmail.com>
2019-10-29 22:45:36 +0900

    기본 함수+버블정렬 주석 추가

--- Algorithm_PyGame.py
+++ Algorithm_PyGame.py
@@ -30,12 +30,18 @@ game_name = pygame.display.set_caption("Learning Algorithm")
 clock = pygame.time.Clock()
 
 
-def drawObject(obj, x, y):
-    global screen
-    screen.blit(obj, (x, y))
-
-
 def writeText(text, x, y, size, font='D2', color=BLACK, rect=None):
+    """
+    텍스트를 스크린에 그리는 함수
+
+    :param text: 쓰고 싶은 문자열
+    :param x: 텍스트 x 좌표
+    :param y: 텍스트 y 좌표
+    :param size: 텍스트 사이즈
+    :param font: 텍스트 폰트(D2, 도현)
+    :param color: 텍스트 색깔
+    :param rect: x, y가 좌측 상단일 경우는 None, 중심일 경우는 center
+    """
     if font == 'D2':
         textfont = pygame.font.Font('D2CodingBold-Ver1.3.2-20180524.ttf', size)
     elif font == '도현':
@@ -51,13 +57,31 @@ def writeText(text, x, y, size, font='D2', color=BLACK, rect=None):
 
 
 def createButton(color, x, y, width, height, line=0):
+    """
+    버튼을 그리는 함수
+
+    :param color: 버튼 색깔
+    :param x: 버튼 x 좌표
+    :param y: 버튼 y 좌표
+    :param width: 버튼 가로 길이
+    :param height: 버튼 세로 길이
+    :param line: 버튼 외곽에 선을 추가할거면 line 변수에 int 입력
+    """
     global screen
     pygame.draw.rect(screen, Dark[color], (x, y, width, height), line)
     mouse = pygame.mouse.get_pos()
     if (x < mouse[0] < x+width) and (y < mouse[1] < y + height):
         pygame.draw.rect(screen, color, (x, y, width, height), line)
 
+
 def checkBlock(total_list):
+    """
+    리스트 내 블록 갯수에 따라 블록 크기를 결정하는 함수
+
+    :param total_list: Block이 들어있는 리스트
+    :return: start_x(왼쪽 공백 길이), start_y(시작 y 좌표),
+             block_size(양 공백과 블록 간 공백을 제외한 나머지 길이를 균등하게 나눈 값), blank(블록 간 공백)
+    """
     global size
     # 앞 뒤로 80px 남기기
     # 계산 공식: (1140 - (간격 * (칸 개수 -1))) / 칸 개수 == 칸의 크기
@@ -116,6 +140,13 @@ def checkBlock(total_list):
 
 
 def drawBlock(total_list, color=BLACK, line=10):
+    """
+    리스트 길이에 맞는 블록 크기를 구하고, 리스트 내 각 블록들을 draw 메서드를 통해 크기에 맞게 스크린에 그려주는 함수
+
+    :param total_list: 블록이 들어있는 리스트
+    :param color: 블록 색깔
+    :param line: 블록 겉테두리(숫자는 두께)
+    """
     (start_x, start_y, block_size, blank) = checkBlock(total_list)
     for i, block in enumerate(total_list):
         block_info = ((start_x)+i*(block_size+blank), start_y, block_size, block_size)
@@ -174,10 +205,17 @@ class Block:
         return "{}".format(self.num)
 
     def draw(self, info, color=BLACK, line=10):
+        """
+        개별 블록을 직접 그리는 메서드
+
+        :param info: 블록이 그려져야할 좌표값(x좌표=info[0], y좌표=info[1])과 블록 사이즈(info[2]), 각 블록 간 공백(info[3])
+        :param color: 블록 색깔
+        :param line: 블록 테두리
+        """
         pygame.draw.rect(screen, color, info, line)
-        num_font = pygame.font.Font('D2CodingBold-Ver1.3.2-20180524.ttf', round(info[2]/2))
-        num_text = num_font.render("{}".format(self.num), True, color)
-        num_text_rect = num_text.get_rect(center=(round((2*info[0]+info[2])/2), round((2*info[1]+info[3])/2)))
+        num_font = pygame.font.Font('D2CodingBold-Ver1.3.2-20180524.ttf', round(info[2]/2)) # 폰트 크기 = (블록 사이즈 / 2)
+        num_text = num_font.render("{}".format(self.num), True, color) # 화면에 그릴 글자는 개별 블록의 숫자
+        num_text_rect = num_text.get_rect(center=(round((2*info[0]+info[2])/2), round((2*info[1]+info[3])/2))) # 블록 중앙
         screen.blit(num_text, num_text_rect)
 
 
@@ -195,6 +233,7 @@ def select_length():
 
 
 def show_bubble(data_list):
+    # 버블정렬을 화면에 그리는 총괄 함수
     global index, bubble_process
     index = 0
     bubble_process = bubble_result(data_list)
@@ -213,6 +252,7 @@ def show_bubble(data_list):
 
 
 def bubble_result(data_list):
+    # 버블 정렬의 순차 과정을 담은 리스트를 return(인덱스 0: 함수 이름, 1: 블록들이 담긴 리스트, 2: tmp(현재 비교 대상)
     global index, bubble_process
     result = []
     for check in range(len(data_list)-1, 0, -1):
@@ -229,6 +269,7 @@ def bubble_result(data_list):
 
 
 def bubble_draw(data_list, tmp, color=BLACK, line=10):
+    # 버블 정렬 중, 기본 상태를 그리는 함수
     global index, bubble_process
     (start_x, start_y, block_size, blank) = checkBlock(data_list)
     while True:
@@ -280,6 +321,7 @@ def bubble_draw(data_list, tmp, color=BLACK, line=10):
 
 
 def bubble_change(data_list, tmp, color=BLACK, line=10):
+    # 버블 정렬 중, 두 값이 교체되는 경우를 그리는 함수
     global index, bubble_process
     (start_x, start_y, block_size, blank) = checkBlock(data_list)
     front_x = (start_x)+tmp*(block_size+blank)
